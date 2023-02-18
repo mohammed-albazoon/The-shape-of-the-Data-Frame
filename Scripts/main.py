9d268a29ef253b788e8509ac86e79beffabc29ca
mohammedalbazoon8 <swadimohammed203@gmail.com>
2023-02-18 03:40:33 -0500

    Importing pandas library to print out the shape of the data frame

---
+++ Scripts/main.py
@@ -0,0 +1,13 @@
+# importing pandas library
+import pandas as pd
+
+# the path of cvs file
+filename = r"../CSV_Files/nba.csv"
+headers = ["Name", "Team", "Number", "Position",
+           "Age", "Height", "Weight", "College", "Salary"]
+
+# Read the cvs file
+df = pd.read_csv(filename, columes=headers)
+
+# To see what the data set looks like, we'll use the head() method.
+df.head()
