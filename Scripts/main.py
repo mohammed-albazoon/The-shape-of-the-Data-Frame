03e446b18c2a3bee1e46a0d4f0ef36537a5dc921
Dylan DeMarco <ddemarco@college.harvard.edu>
2023-02-22 13:38:10 -0500

    BF-Remove regex compilation and correct file path--paths are relative from where you run the file

--- Scripts/main.py
+++ Scripts/main.py
@@ -1,8 +1,11 @@
 # importing pandas library
 import pandas as pd
 
-# the path of cvs file
-filename = r"../CSV_Files/nba.csv"
+# the path of csv depends on whether you run the script from the same directory or not
+# if you run 'python main.py' from the same directory, you can use the following path
+# filename = "../CSV_Files/nba.csv"
+# if you run 'python Scripts/main.py' from the root directory, you can use the following path
+filename = "CSV_Files/nba.csv"
 headers = ["Name", "Team", "Number", "Position",
            "Age", "Height", "Weight", "College", "Salary"]
 
