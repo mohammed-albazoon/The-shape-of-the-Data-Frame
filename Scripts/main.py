3f1d40824fbf8cd4624401b0adbcf0abea91229d
Dylan DeMarco <ddemarco@college.harvard.edu>
2023-02-22 13:44:25 -0500

    BF-Remove headers arg because they are already present in the file to prevent duplication

--- Scripts/main.py
+++ Scripts/main.py
@@ -6,11 +6,9 @@ import pandas as pd
 # filename = "../CSV_Files/nba.csv"
 # if you run 'python Scripts/main.py' from the root directory, you can use the following path
 filename = "CSV_Files/nba.csv"
-headers = ["Name", "Team", "Number", "Position",
-           "Age", "Height", "Weight", "College", "Salary"]
 
 # Read the cvs file
-df = pd.read_csv(filename, names=headers)
+df = pd.read_csv(filename)
 
 # To see what the data set looks like, we'll use the head() method.
 print(df.head())
